03f57463a81252952ddd95d61e2bfa1f04ff456d
Gonzalo Escamilla <escamillagonzalo@gmail.com>
2023-03-22 10:37:13 -0300

    Initial commit

---
+++ Assets/Scripts/SomeScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeScript : MonoBehaviour
+{
+    public void MethodA()
+    {
+        Debug.Log("Some logging data A");
+        Debug.Log("Some logging data A");
+        Debug.Log("Some logging data A");
+        Debug.Log("Some logging data A");
+        Debug.Log("Some logging data A");
+        Debug.Log("Some logging data A");
+    }
+
+    public void MethodB()
+    {
+        Debug.Log("Some logging data B");
+        Debug.Log("Some logging data B");
+        Debug.Log("Some logging data B");
+        Debug.Log("Some logging data B");
+        Debug.Log("Some logging data B");
+        Debug.Log("Some logging data B");
+    }
+}
